f9ef3daa4ce13f8712e70ea73072480740d7bb64
Mike Henderson <henderson.mike.j@gmail.com>
2021-04-23 02:11:41 -0500

    integrating graphs

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from SentimentModel import SentimentModel
 from TweetScraper import TweetScraper
 import argparse
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def initArgs():
-    parser = argparse.ArgumentParser();
+    parser = argparse.ArgumentParser()
     parser.add_argument('-username', help='Username to be searched on')
     parser.add_argument('-keyword', help='Keyword to be search on')
     parser.add_argument('-limit', help='Max number of results returned')
@@ -36,8 +37,25 @@ if __name__ == '__main__':
 
     for index, row in df.iterrows():
         isPositive = analyzer.classify(row['tweet'])
-        result.append((row['tweet'], row['date'], isPositive))
 
-    resultDf = pd.DataFrame(result, columns=['Tweet', 'Date', 'IsPositive'])
+        if isPositive:
+            score = 1
+        else:
+            score = -1
 
-    print(resultDf)
+        result.append((row['date'], score))
+
+    resultDf = pd.DataFrame(result, columns=['date', 'positivity_score'])
+    resultDf['date'] = pd.to_datetime(resultDf['date'])
+
+    aggDf = resultDf.groupby(pd.Grouper(key='date', freq='W')).sum()
+
+    if args.username:
+        plt.title(f'Overall Sentiment for User @{args.username}')
+    else:
+        plt.title(f'Overall Sentiment for keyword {args.keyword}')
+
+    plt.xlabel('Date')
+    plt.ylabel('Sentiment Score')
+    plt.plot(aggDf)
+    plt.show()
